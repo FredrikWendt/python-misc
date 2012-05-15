933c07d079affde75633934ab58d1f015aa08b79
Fredrik Wendt <fredrik@wendt.se>
2012-05-15 10:53:18 +0200

    updated to exit on non-0 exit code

--- doit/doit.sh
+++ doit/doit.sh
@@ -1,12 +1,24 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 BASE=`pwd`
 
 for i in `cat projects.txt` ; do
 
 	echo $i
-	cd "$BASE/$i"
-	"$@"
+	cd "$BASE"
+	if [ -d "$i" ] ; then
+		cd "$i"
+		"$@"
+		if [ $? -gt 0 ] ; then
+			if [ "$1 $2" != "hg out" ] ; then
+				echo "Aborting!"
+				exit $? 
+			fi
+		fi
+	else
+		echo "Skipping - no such directory"
+	fi
 	echo
 
 done
+
